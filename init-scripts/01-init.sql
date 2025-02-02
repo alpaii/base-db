98b67542c69a15ef2e5f438530eda46b0597353e
jkh <jkh@abc.com>
2025-02-02 16:58:28 +0900

    add comments

--- init-scripts/01-init.sql
+++ init-scripts/01-init.sql
@@ -1,4 +1,5 @@
--- 사용자 생성 및 권한 부여
+-- 사용자 생성 및 권한 부여.
+-- base를 새로운 프로젝트 db 이름으로 수정
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'userpassword';
 GRANT ALL PRIVILEGES ON base.* TO 'user'@'%';
 FLUSH PRIVILEGES;
