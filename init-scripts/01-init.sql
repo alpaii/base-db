3bf9b717e59f33c7bf0e7e15b52b239d23f9fa94
jkh <jkh@abc.com>
2025-02-02 17:10:26 +0900

    english

--- init-scripts/01-init.sql
+++ init-scripts/01-init.sql
@@ -1,5 +1,4 @@
--- 사용자 생성 및 권한 부여.
--- base를 새로운 프로젝트 db 이름으로 수정
+-- Replace base with your new project db name.
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'userpassword';
 GRANT ALL PRIVILEGES ON base.* TO 'user'@'%';
 FLUSH PRIVILEGES;
