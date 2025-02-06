eb722e0121dad951fa5b6b3d3cf354eeb6c4af20
alpaii <112483827+alpaii@users.noreply.github.com>
2025-02-06 16:30:38 +0900

    Update 01-init.sql

--- init-scripts/01-init.sql
+++ init-scripts/01-init.sql
@@ -2,3 +2,7 @@
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'userpassword';
 GRANT ALL PRIVILEGES ON base.* TO 'user'@'%';
 FLUSH PRIVILEGES;
+
+-- test_base for pytest
+GRANT ALL PRIVILEGES ON test_base.* TO 'user'@'%' IDENTIFIED BY 'userpassword';
+FLUSH PRIVILEGES;
