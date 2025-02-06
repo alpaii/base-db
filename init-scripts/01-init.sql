a80b6d9f05d5243175af80046cc49f8a4c95a6cf
alpaii <112483827+alpaii@users.noreply.github.com>
2025-02-06 17:07:08 +0900

    Update 01-init.sql

--- init-scripts/01-init.sql
+++ init-scripts/01-init.sql
@@ -1,8 +1,9 @@
--- Replace base with your new project db name.
+-- Replace "base" with your new project db name.
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'userpassword';
 GRANT ALL PRIVILEGES ON base.* TO 'user'@'%';
 FLUSH PRIVILEGES;
 
 -- test_base for pytest
-GRANT ALL PRIVILEGES ON test_base.* TO 'user'@'%' IDENTIFIED BY 'userpassword';
+CREATE DATABASE IF NOT EXISTS test_base CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL PRIVILEGES ON test_base.* TO 'user'@'%';
 FLUSH PRIVILEGES;
